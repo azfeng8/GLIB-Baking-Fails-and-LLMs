756e66c1115d147f80ca0d722a4f2509e415c5dc
Annie Feng <azfeng8@gmail.com>
2023-12-11 17:40:09 -0500

    implement LLM initialization
    - auto create init prompt from operator names and list of predicates
    - implement a safeguard to check argument types and valid names
    
    next: how to drop individual predicates instead of the whole operator.

--- operator_learning_modules/zpk/zpk_operator_learning.py
+++ operator_learning_modules/zpk/zpk_operator_learning.py
@@ -5,7 +5,9 @@ from pddlgym.structs import TypedEntity, ground_literal
 from ndr.learn import run_main_search as learn_ndrs
 from ndr.learn import get_transition_likelihood, print_rule_set, iter_variable_names
 from ndr.ndrs import NOISE_OUTCOME, NDR, NDRSet
+import re
 import openai
+import os
 
 from collections import defaultdict
 import tempfile
@@ -136,6 +138,8 @@ class LLMZPKOperatorLearningModule(ZPKOperatorLearningModule):
         #     self._fits_all_data[action.predicate] = False
         # self.learn()
 
+        self._openai = openai.OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+
         # # Initialize the operators from the LLM.
         prompt = self._create_prompt()
         llm_output = self._query_llm(prompt)
@@ -163,187 +167,166 @@ class LLMZPKOperatorLearningModule(ZPKOperatorLearningModule):
             ndrs = NDRSet(action, [ndr])
             self._ndrs[action.predicate] = ndrs
 
+    def _create_header(self):
+        """Create the PDDL Domain header."""
+        env = ac.train_env
+        preds = [p for p in env.action_space.predicates] + [p for p in env.observation_space.predicates]
+        types = set()
+        lines = []
+        for p in preds:
+            types |= set(p.var_types)
+            s = f"({p.name} " + " ".join(p.pddl_variables()) + ")"
+            lines.append(s)
+        predicates = "(:predicates\n" + "\n".join(lines) + ")"
+        actions = '; (:actions ' + ' '.join([p.name for p in env.action_space.predicates]) + ")"
+        types = "(:types " + " ".join(types) + ")"
+
+        return f"(define (domain {self._domain_name.lower()})\n(:requirements :typing)\n{types}\n{predicates}\n\n{actions}"
+
     def _create_prompt(self):
-        # TODO: use ac.train_env to extract predicates, operator names, and
-        # create this prompt automatically.
-        """"""
-        assert self._domain_name == "Glibblocks"
-        # reference: https://github.com/tomsilver/pddlgym/tree/master/pddlgym/pddl/glibblocks.pddl
-        prompt = """# Fill in the <TODO> to complete the PDDL domain.
-        
-        (define (domain glibblocks)
-    (:requirements :strips :typing)
-    (:types block robot)
-    (:predicates 
-        (on ?x - block ?y - block)
-        (ontable ?x - block)
-        (clear ?x - block)
-        (handempty ?x - robot)
-        (handfull ?x - robot)
-        (holding ?x - block)
-        (pickup ?x - block)
-        (putdown ?x - block)
-        (stack ?x - block ?y - block)
-        (unstack ?x - block)
-    )
+        """Generate the prompt using operator names, the action parameters (a subset of operator parameters), object types, and the observation predicates with types.
+        """
+        #Create the PDDL domain header without the extra syntax and action predicates that may confuse the LLM
+        types = set()
+        lines = []
+        for p in ac.train_env.observation_space.predicates:
+            types |= set(p.var_types)
+            s = f"({p.name} " + " ".join(p.pddl_variables()) + ")"
+            lines.append(s)
+        predicates = "(:predicates\n\t\t" + "\n\t\t".join(lines) + "\n\t)"
+        types = "(:types " + " ".join(types) + ")"
+        header = f"(define (domain {self._domain_name.lower()})\n\t{types}\n\t{predicates}\n"
 
-    ; (:actions pickup putdown stack unstack)
-
-    (:action pick-up
-        :parameters (?x - block <TODO>)
-        :precondition (and
-            (pickup ?x) 
-            <TODO>
-        )
-        :effect (and
-            <TODO>
-        )
-    )
-
-    (:action put-down
-        :parameters (?x - block <TODO>)
-        :precondition (and 
-            (putdown ?x)
-            <TODO>
-        )
-        :effect (and 
-            <TODO>
-        )
-
-    (:action stack
-        :parameters (?x - block ?y - block <TODO>)
-        :precondition (and
-            (stack ?x ?y)
-            <TODO>
-        )
-        :effect (and 
-            <TODO>
-        )
-    )
-
-    (:action unstack
-        :parameters (?x - block <TODO>)
-        :precondition (and
-            (unstack ?x)
-            <TODO>
-        )
-        :effect (and 
-            <TODO>
-        )
-    )
-)"""
+        prompt = "# Fill in the <TODO> to complete the PDDL domain.\n" + header  + "\n"
+        for action_pred in ac.train_env.action_space.predicates:
+            s = f"\t(:action {action_pred.name}\n"
+            s += "\t\t:parameters (" + " ".join(action_pred.pddl_variables()) + " <TODO>)\n"
+            s += f"\t\t:precondition (and\n\t\t\t\t<TODO>\n\t\t)\n"
+            s += f"\t\t:effect (and\n\t\t\t\t<TODO>\n\t\t)\n\t)"
+            prompt += s + "\n"
+        prompt += ")"
+        print("Prompt:", prompt)
 
         return prompt
 
     def _query_llm(self, prompt):
-        # TODO cache and make settings
+        # TODO: cache and make settings
         # reference: https://github.com/Learning-and-Intelligent-Systems/llm4pddl/blob/main/llm4pddl/llm_interface.py
 
         # TODO: uncomment. Leaving commented for now to avoid spurious queries
         # of the expensive open AI API. Also we might want to use ChatGPT instead...
-        # completion = openai.Completion.create(
-        #     engine="code-davinci-002",
-        #     prompt=prompt,
-        #     max_tokens=500,
-        #     temperature=0,
-        # )
-        # response = completion.choices[0].text
-
-        # This is a response from ChatGPT (manually collected)
-        response = """(define (domain glibblocks)
-(:requirements :strips :typing)
-(:types block robot)
-(:predicates
-(on ?x - block ?y - block)
-(ontable ?x - block)
-(clear ?x - block)
-(handempty ?x - robot)
-(handfull ?x - robot)
-(holding ?x - block)
-(pickup ?x - block)
-(putdown ?x - block)
-(stack ?x - block ?y - block)
-(unstack ?x - block)
-)
-
-; (:actions pickup putdown stack unstack)
-
-(:action pick-up
-    :parameters (?r - robot ?x - block)
-    :precondition (and
-        (pickup ?x) 
-        (handempty ?r)
-        (clear ?x)
-    )
-    :effect (and
-        (handfull ?r)
-        (not (handempty ?r))
-        (not (clear ?x))
-        (holding ?x)
-    )
-)
-
-(:action put-down
-    :parameters (?r - robot ?x - block)
-    :precondition (and 
-        (putdown ?x)
-        (handfull ?r)
-        (holding ?x)
-    )
-    :effect (and 
-        (handempty ?r)
-        (not (handfull ?r))
-        (clear ?x)
-        (not (holding ?x))
-    )
-)
-
-(:action stack
-    :parameters (?r - robot ?x - block ?y - block)
-    :precondition (and
-        (stack ?x ?y)
-        (handfull ?r)
-        (holding ?x)
-        (clear ?y)
-    )
-    :effect (and 
-        (handempty ?r)
-        (not (handfull ?r))
-        (not (holding ?x))
-        (not (clear ?y))
-        (on ?x ?y)
-    )
-)
-
-(:action unstack
-    :parameters (?r - robot ?x - block)
-    :precondition (and
-        (unstack ?x)
-        (handempty ?r)
-        (on ?x ?y)
-    )
-    :effect (and 
-        (handfull ?r)
-        (not (handempty ?r))
-        (clear ?x)
-        (not (on ?x ?y))
-        (holding ?x)
-    )
-)
-
-)
-"""
-
+        completion = self._openai.chat.completions.create(
+            model="gpt-4",
+            messages = [{"role":"user", "content": prompt}],
+            max_tokens=4096,
+            temperature=0,
+        )
+        response = completion.choices[0].text
+        print("Got response", response)
         return response
+    
+    def _check_operator_string(self, types, operator_str):
+        """
+        Parses the PDDL operator from the string and returns False if the operator string is malformed.
+
+        Args:
+            types (set[str]): set of object types allowed in this domain
+            operator_str (str): string containing a PDDL operator definition
+        Returns:
+            bool, str: returns False, error_msg if the string is malformed.
+            
+        """
+        action_name = re.search("[\w]+", operator_str).group(0)
+        action_preds = {p.name:p for p in ac.train_env.action_space.predicates}
+        if action_name not in action_preds:
+            return False,f"Invalid action found: {action_name}"
+        # assert action_name in action_preds, f"Invalid action found: {action_name}"
+        parameter_str = re.search("\:parameters[^\)]*\)", operator_str).group(0)
+        pddl_variables = re.findall("\?[\w\d\s\-]+", parameter_str)
+        param_names = []
+        param_types = []
+        for var_str in pddl_variables:
+            var_str = var_str.strip()
+            param_name = re.search("\?[\w\d]+", var_str).group(0)[1:] # trim the ? in beginning
+            var_type = re.search("\-\s[\w]+", var_str).group(0)[2:] # trim the "- " in the beginning
+            param_names.append(param_name)
+            param_types.append(var_type)
+            if var_type not in types:
+                return False, f"type not found: {var_type} in parameters: {parameter_str}"
+            # assert var_type in types, f"type not found: {var_type} in parameters: {parameter_str}"
+
+        # Check each precondition and effect have 1. valid predicate name, 2.predicate argument names match params, 3. predicate argument types match params 
+        obs_preds = {o.name:o for o in ac.train_env.observation_space.predicates}
+        precond_str = re.search(":precondition([\s\S]*?):effect", operator_str).group(0)[:-7] # trim the ":effect" at the end
+        effect_str = re.search(":effect([\s\S]*?)\s\)", operator_str).group(0)
+        for pre_or_eff in [precond_str, effect_str]:
+            for predicate_str in re.findall("\([\w]+[\s\?\w]*", pre_or_eff):
+                if "and" in predicate_str or "not" in predicate_str: continue
+                pred_name = re.search("[\w]+", predicate_str).group(0)
+                if pred_name != action_name: # extra predicate in preconditions to differ operators vs. actions in PDDLGym
+                    if pred_name not in obs_preds:
+                        return False, f"Predicate name not valid: {pred_name}, in precondition {pre_or_eff}"
+                    # assert pred_name in obs_preds, f"Predicate name not valid: {pred_name}, in precondition {pre_or_eff}"
+                arg_types = []
+                for arg_name in re.findall("\?[\w\d]+", predicate_str):
+                    arg_name = arg_name[1:] # remove the "?" in front
+                    if arg_name not in param_names:
+                        return False,f"Argument for {pred_name} in {pre_or_eff} not in parameters: {arg_name}"
+                    # assert arg_name in param_names, f"Argument for {pred_name} in {pre_or_eff} not in parameters: {arg_name}"
+                    arg_types.append(param_types[param_names.index(arg_name)])
+                if pred_name == action_name:
+                    if arg_types != action_preds[pred_name].var_types:
+                        return False, f"Types don't match for predicate {pred_name}: {arg_types} vs. {action_preds[pred_name].var_types}"
+                    # assert arg_types == action_preds[pred_name].var_types, f"Types don't match for predicate {pred_name}: {arg_types} vs. {action_preds[pred_name].var_types}"
+                else:
+                    if arg_types != obs_preds[pred_name].var_types:
+                        return False,f"Types don't match for predicate {pred_name} in action {action_name}: {arg_types} vs. {obs_preds[pred_name].var_types}"
+                    # assert arg_types == obs_preds[pred_name].var_types, f"Types don't match for predicate {pred_name} in action {action_name}: {arg_types} vs. {obs_preds[pred_name].var_types}"
+        return True, ""
 
     def _llm_output_to_operators(self, llm_output):
         # Parse the LLM output using PDDLGym.
         
-        # TODO: automatically handle this and other cases of malformed LLM output.
-        # In this case, we need to add a missing parameter.
-        llm_output =llm_output.replace("""(:action unstack
-    :parameters (?r - robot ?x - block)""", """(:action unstack
-    :parameters (?r - robot ?x - block ?y - block)""")
+        # Automatically detect malformed LLM output.
+        # To correct LLM output, currently drop the predicates whose types do not match.
+
+        # Split the response into chunks separated by "(:action ", and discard the first chunk with the header and LLM chat.
+        operator_matches = list(re.finditer("\(\:action\s", llm_output))
+
+        action_pddls = []
+
+        # Collect the object types in this domain.
+        types = set()
+        for p in ac.train_env.action_space.predicates:
+            for t in p.var_types:
+                types.add(t)
+
+        end = len(llm_output)
+        # Check each action's preconditions, parameters, and effects for valid names and consistent typing.
+        for match in operator_matches[::-1]: # Read matches from end of file to top of file
+            start = match.end()
+            operator_str = llm_output[start:end]
+            success, debug_msg = self._check_operator_string(types, operator_str)
+            if success:
+                # Count parantheses: look for the closing to "(:action" to get the operator string.
+                open = 0
+                close = 0
+                i = start
+                for c in operator_str:
+                    if close > open:
+                        action_pddls.append("(:action " + llm_output[start:i])
+                        break
+                    if c == "(":
+                        open += 1
+                    elif c == ")":
+                        close += 1
+                    i+=1
+            
+            end = match.start()
+
+
+        header = self._create_header()
+        llm_output = header + "\n".join(action_pddls)
 
         domain_fname = tempfile.NamedTemporaryFile(delete=False).name
         with open(domain_fname, "w", encoding="utf-8") as f:
