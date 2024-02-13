388d58a7710437a253a38529be357313cdcad3dd
Nishanth Kumar <nishanth.kumar20@gmail.com>
2024-02-13 17:49:37 -0500

    almost ready to launch; once logging is reconfigured, should be able to finish up!

--- README.md
+++ README.md
@@ -25,27 +25,29 @@ Run `python main.py ...`.
 
 ## Required Arguments:
 
---domains : str, + :
+`--domains : str, +` :
 
 list of PDDLGym domains.
 
---curiosity_methods : str, +:
+`--curiosity_methods : str, +` :
 
 list of curiosity methods. See settings.py for the complete list in AgentConfig.curiosity_methods_to_run.
 
---learning_name : str:
+`--learning_name : str` :
 
 name of the learning method. See settings.py for the complete list in AgentConfig.learning_name. 
 
---start_seed : int:
+`--start_seed : int` :
 
 starting seed number of the first seed. The following seeds increment from here.
 
---num_seeds : int:
+`--num_seeds : int` :
 
 number of seeds to run.
 
 
-### An example command is:
+### Example Command:
 
+```
 python main.py --domains Baking  --curiosity_methods LLM+GLIB_G1  --learning_name LLMIterative+ZPK  --start_seed 40 --num_seeds 1
+```
--- openstack_scripts/launch.py
+++ openstack_scripts/launch.py
@@ -3,12 +3,13 @@
 Requires a file that contains a list of IP addresses for instances that are:
     - Turned on
     - Accessible via ssh for the user of this file
-    - Configured with a predicators image
+    - Configured with an llm_glib image (current snapshot name: llm_glib-v1)
     - Sufficient in number to run all of the experiments in the config file
-
+Make sure to place this file within the openstack_scripts folder.
+    
 Usage example:
-    python openstack_scripts/openstack/launch.py --config example_basic.yaml \
-        --machines machines.txt --sshkey ~/.ssh/cloud.key
+    python openstack_scripts/launch.py --config example.yaml \
+        --machines machines.txt
 
 The default branch can be overridden with the --branch flag.
 """
@@ -16,7 +17,7 @@ The default branch can be overridden with the --branch flag.
 import argparse
 import os
 
-from openstack_scripts.cluster_utils import DEFAULT_BRANCH, SingleSeedRunConfig, \
+from cluster_utils import DEFAULT_BRANCH, SingleSeedRunConfig, \
     config_to_cmd_flags, config_to_logfile, generate_run_configs, \
     get_cmds_to_prep_repo, run_cmds_on_machine
 
@@ -26,7 +27,7 @@ def _main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument("--config", required=True, type=str)
     parser.add_argument("--machines", required=True, type=str)
-    parser.add_argument("--sshkey", required=True, type=str)
+    parser.add_argument("--sshkey", required=False, type=str, default=None)
     parser.add_argument("--branch", type=str, default=DEFAULT_BRANCH)
     args = parser.parse_args()
     openstack_dir = os.path.dirname(os.path.realpath(__file__))
@@ -35,7 +36,8 @@ def _main() -> None:
     with open(machine_file, "r", encoding="utf-8") as f:
         machines = f.read().splitlines()
     # Make sure that the ssh key exists.
-    assert os.path.exists(args.sshkey)
+    if args.sshkey is not None:
+        assert os.path.exists(args.sshkey)
     # Generate all of the run configs and make sure that we have enough
     # machines to run them all.
     run_configs = list(generate_run_configs(args.config))
@@ -46,15 +48,15 @@ def _main() -> None:
         assert isinstance(cfg, SingleSeedRunConfig)
         logfile = os.path.join("logs", config_to_logfile(cfg))
         cmd_flags = config_to_cmd_flags(cfg)
-        cmd = f"python3.8 predicators/main.py {cmd_flags}"
+        cmd = f"python main.py {cmd_flags}"
         _launch_experiment(cmd, machine, logfile, args.sshkey, args.branch)
 
 
 def _launch_experiment(cmd: str, machine: str, logfile: str, ssh_key: str,
                        branch: str) -> None:
     print(f"Launching on machine {machine}: {cmd}")
-    # Enter the repo.
-    server_cmds = ["cd ~/predicators"]
+    # Enter the repo and activate conda.
+    server_cmds = ["llm_glib"]
     # Prepare the repo.
     server_cmds.extend(get_cmds_to_prep_repo(branch))
     # Run the main command.
