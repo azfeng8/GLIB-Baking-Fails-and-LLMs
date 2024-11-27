b817ad13b009dfcaac8719bcb59687f881b49dae
azfeng8 <azfeng8@gmail.com>
2024-11-27 13:01:00 -0500

    solved problem 4

--- realistic-baking/llm_query_plans/interact_pddlgym.py
+++ realistic-baking/llm_query_plans/interact_pddlgym.py
@@ -7,6 +7,7 @@ DONE: update the pipeline to be able to revise the plan sketch while grounding o
 DONE: update domain because the plan for souffle mixture is correct but not executing.
 
 TODO: update the pipeline to handle feedback from failed plan executions.
+TODO: allow environment reset to the very beginning and replan.
 """
 import numpy as np
 import pickle
@@ -54,15 +55,34 @@ def get_facts(descriptions, state_string):
 
 
 class LLMAgent:
+    """
+    
+    Attributes:
+        self.plan_sketch_conversation: The conversation where planning mistakes are discussed and in which the most recent plan sketch is the last response in. 
+    """
     def __init__(self, env):
         """Create the prompting agent.
         """
         self.llm = OpenAI_Model()
         self.env = env
         self.action_preds = {p.name: p for p in env.action_space.predicates}
-        # self.plan = []
         self.action_name_sequence = []
+        self.actions_done_descriptions = []
         self._get_intro()
+
+        with open('predicate_and_goal_descriptions.json', 'r') as f:
+            descriptions = json.load(f)
+        self.descriptions = descriptions
+
+        action_description_string = ""
+        action_names_string = ""
+        for k, v in descriptions["lifted_skill_descriptions"].items():
+            action_description_string += k + ": " + v + '\n'
+            action_names_string += k  + '\n'
+        self.action_description_string = action_description_string
+ 
+        self.planning_attempts = 0
+        self.action_number = 0
    
     def _get_intro(self):
         conversation = []
@@ -74,11 +94,25 @@ class LLMAgent:
 
         Since you are baking desserts, first determine what are the differences between a cake and sweet, light, and airy souffle. Please rationalize what are the essential ingredients and their amounts to make those desserts and use only those. Please note that our souffles require a very small amount of flour.
         """
-        # print("**********************PROMPT********************")
-        # print(intro_prompt)
-        conversation.append( {"role": "user", "content": intro_prompt})
-        responses, _ = self.llm.sample_completions(conversation, num_completions=1, temperature=0, seed=1)
-        conversation.append({"role": "assistant", 'content': responses[0]})
+        self._query_llm(intro_prompt, None, conversation)
+
+        types_prompt = \
+        """In the kitchen, there different kinds of objects that you can interact with. The different kind of objects that you see are categorized into the following:
+
+        container
+        measuring cup
+        dessert
+        powder
+        butter
+        mixture
+        egg
+        oven
+        spatula
+        electric stand mixer
+
+        Right now, you see the some of these ingredients and items on the counter. You also see some appliances in the kitchen. 
+        """
+        self._query_llm(types_prompt, None, conversation)
         self.intro_conversation = conversation
 
 
@@ -87,23 +121,72 @@ class LLMAgent:
         self.instruction_steps = []
         self.plan_sketch_conversation = None
         self.action_number = 0
+        self.planning_attempts += 1
+        self.actions_done_descriptions = []
     
-    def give_plan_failure_feedback(self):
-        prompt = f"""Make sure that you are combining the right ingredients in the right way, making sure that the entire plan makes sense."""
-        raise NotImplementedError()
+    def give_plan_failure_feedback_and_replan(self, obs, problem_idx):
+        """Append the plan failure feedback prompt to the conversation, and plan again with the failed plan in the context."""
+        CONVO_SAVE_PATH = os.path.join(SAVE_PATH, f'problem{problem_idx}', f'plan_attempt_{self.planning_attempts}', f'action0.pkl')
+        executed_plan_string = []
+        for i, description in enumerate(self.actions_done_descriptions):
+            executed_plan_string.append(f'{i+1}. {description}' )
+        last_action_description = executed_plan_string[-1]
+        executed_plan_string = '\n'.join(executed_plan_string)
 
-    def get_action(self, obs, problem_idx, planning_attempts):
+        with open('predicate_and_goal_descriptions.json', 'r') as f:
+            descriptions = json.load(f)
+        state = ''
+        for lit in obs.literals:
+            if lit.predicate.name not in ('different', 'name-less-than') and lit.predicate not in self.env.action_space.predicates:
+                state += lit.pddl_str() + '\n'
+        state_description = get_facts(descriptions, state)
+        failure_prompt = f"""Based on your plan, we've just executed these actions:""" +  executed_plan_string + \
+        f"""However, the last action failed to execute properly. Before we executed the last action, the following facts were true in the environment:
+
+        {state_description}
+
+        Then, we tried executing this action:
+        
+        {last_action_description}
+
+        However, executing this action failed. Please explain what happened.
+        """
+        self._query_llm(failure_prompt, CONVO_SAVE_PATH, self.plan_sketch_conversation)
+        print("Plan failure explanation at: ", CONVO_SAVE_PATH)
+
+        replan_prompt = \
+        f"""
+        Ok, thanks for the explanation. Now, let's replan to the goal from the beginning and avoid this mistake and all previous mistakes.
+
+        These are the names of the atomic actions that we can perform, along with their descriptions:
+        {self.action_description_string}
+
+        Can you please give a sequence of these phrases that will get us to the goal? Include the exact phrase in each step of your answer. Format it using a numbered list with one line per step, starting with "1.". Give a little explanation of each step underneath each bullet point. Mark the end of the plan with '***' in your response. Please avoid all past planning mistakes.
+        """
+        self._query_llm(replan_prompt, CONVO_SAVE_PATH, self.plan_sketch_conversation)
+        CONVO_SAVE_PATH = os.path.join(SAVE_PATH, f'problem{problem_idx}', f'plan_attempt_{self.planning_attempts}', f'action0.pkl')
+        conv = self.plan_sketch_conversation
+        self.reset_plan()
+        self.action_name_sequence, self.instruction_steps, self.plan_sketch_conversation = self._parse_plan_sketch_from_LLM(conv, CONVO_SAVE_PATH)
+
+    def get_action(self, obs, problem_idx):
         """Prompt LLM for the next action.
 
         Args:
             obs (pddlgym.structs.State): observation.
         """
         if len(self.action_name_sequence) == 0:
-            self.action_name_sequence, self.instruction_steps, self.plan_sketch_conversation  = self._query_LLM_for_action_name_sequence(obs, problem_idx, planning_attempts)
-            self.action_number = 0
+            self.action_name_sequence, self.instruction_steps, self.plan_sketch_conversation  = self._query_LLM_for_action_name_sequence(obs, problem_idx)
         action_name = self.action_name_sequence.pop(0)
         instruction = self.instruction_steps.pop(0)
-        action = self._query_LLM_for_action(obs, problem_idx, planning_attempts, action_name, instruction, self.plan_sketch_conversation, self.action_number)
+        action = None
+        while action is None:
+            action, action_description = self._query_LLM_for_action(obs, problem_idx, action_name, instruction, self.plan_sketch_conversation, self.action_number)
+            if action is None:
+                # Replan.
+                self.reset_plan()
+                self.action_name_sequence, self.instruction_steps, self.plan_sketch_conversation  = self._query_LLM_for_action_name_sequence(obs, problem_idx)
+        self.actions_done_descriptions.append(action_description)
         self.action_number += 1
         return action
 
@@ -118,40 +201,28 @@ class LLMAgent:
         conversation.append({"role": "user", "content": prompt})
         responses, _ = self.llm.sample_completions(conversation, num_completions=1, temperature=0, seed=1)
         conversation.append({"role": "assistant", 'content': responses[0]})
-        self._save_conversation(conversation, convo_save_path)
+        if convo_save_path is not None:
+            self._save_conversation(conversation, convo_save_path)
 
-    def _query_LLM_for_action_name_sequence(self, obs, problem_idx, planning_attempts):
-        """Queries LLM and returns the ground plan.
+    def _query_LLM_for_action_name_sequence(self, obs, problem_idx):
+        """Queries LLM and returns the action names.
         """
+        planning_attempts = self.planning_attempts
         conversation = deepcopy(self.intro_conversation) # Conversation with the LLM for this action
-        with open('predicate_and_goal_descriptions.json', 'r') as f:
-            descriptions = json.load(f)
+
         initial_state = ''
         for lit in obs.literals:
             if lit.predicate.name not in ('different', 'name-less-than') and lit.predicate not in self.env.action_space.predicates:
                 initial_state += lit.pddl_str() + '\n'
-        initial_state_predicate_fstrings = get_facts(descriptions, initial_state)
+        initial_state_predicate_fstrings = get_facts(self.descriptions, initial_state)
         problem_name = f'problem{problem_idx+1}'
-        goal_state_predicate_fstrings = descriptions['train_goals'][problem_name]
+        goal_state_predicate_fstrings = self.descriptions['train_goals'][problem_name]
 
         action_i = 0
         CONVO_SAVE_PATH = os.path.join(SAVE_PATH, f'problem{problem_idx}', f'plan_attempt_{planning_attempts}', f'action{action_i}.pkl')
         problem_setting_prompt = \
         f"""
-        In the kitchen, there different kinds of objects that you can interact with. The different kind of objects that you see are categorized into the following:
-
-        container
-        measuring cup
-        dessert
-        powder
-        butter
-        mixture
-        egg
-        oven
-        spatula
-        electric stand mixer
-
-        Right now, you see the some of these ingredients and items on the counter. You also see some appliances in the kitchen. The following things are true at this moment:
+        The following things are true at this moment:
 
         {initial_state_predicate_fstrings}
 
@@ -163,10 +234,9 @@ class LLMAgent:
         # print(problem_setting_prompt)
         self._query_llm(problem_setting_prompt, CONVO_SAVE_PATH, conversation)
 
-        action_description_string = ""
+        action_description_string = self.action_description_string
         action_names_string = ""
-        for k, v in descriptions["lifted_skill_descriptions"].items():
-            action_description_string += k + ": " + v + '\n'
+        for k, v in self.descriptions["lifted_skill_descriptions"].items():
             action_names_string += k  + '\n'
         formalizing_intro = \
         f"""These are the things that you would like to become true:
@@ -177,10 +247,15 @@ class LLMAgent:
         These are the names of the atomic actions that we can perform, along with their descriptions:
         {action_description_string}
 
-        Can you please give a sequence of these phrases that will get us to the goal? Include the exact phrase in each step of your answer. Format it using a numbered list with one line per step, starting with "1.". Give a little explanation of each step underneath each bullet point. Mark the end of the plan with '***' in your response.
+        Can you please give a sequence of these phrases that will get us to the goal? Include the exact phrase in each step of your answer. Format it using a numbered list with one line per step, starting with "1.". Give a little explanation of each step underneath each bullet point. Mark the end of the plan with '***' in your response. Please avoid any past planning mistakes.
         """
         self._query_llm(formalizing_intro, CONVO_SAVE_PATH, conversation)
+        return self._parse_plan_sketch_from_LLM(conversation, CONVO_SAVE_PATH)
+    
+    def _parse_plan_sketch_from_LLM(self, conversation, CONVO_SAVE_PATH):
+        """Assumes that the response the parse is the last one in the conversation."""
         loop = True
+        first_plan_sketch_index = len(conversation) - 1 # The last response is the plan sketch
         while loop:
             response =  conversation[-1]['content']
             instruction_steps = []
@@ -199,6 +274,7 @@ class LLMAgent:
                 else:
                     instruction = response[match.start():match.end() - len('2.')]
                 assert match is not None, f'Parsing failed for finding step 1 in plan: {response}'
+                instruction = instruction.lower()
                 action_name = None
                 for action_pred_name in self.action_preds:
                     if action_pred_name in instruction:
@@ -213,11 +289,14 @@ class LLMAgent:
                 action_name_seq.append(action_name)
             if action_name is None:
                 continue
-            return action_name_seq, instruction_steps, conversation
+            else:
+                # If reached here, then done.
+                # modify the conversation so that it only contains the correct plan sketch.
+                conversation = conversation[:first_plan_sketch_index] + [conversation[-1]]
+                return action_name_seq, instruction_steps, conversation
         
-    def _query_LLM_for_action(self, obs, problem_idx, planning_attempts, action_name, instruction, conversation, action_i):
-        with open('predicate_and_goal_descriptions.json', 'r') as f:
-            descriptions = json.load(f)
+    def _query_LLM_for_action(self, obs, problem_idx, action_name, instruction, conversation, action_i):
+        descriptions = self.descriptions
         initial_state = ''
         for lit in obs.literals:
             if lit.predicate.name not in ('different', 'name-less-than') and lit.predicate not in self.env.action_space.predicates:
@@ -228,9 +307,7 @@ class LLMAgent:
 
         plan_sketch_convo = conversation
         conversation = None
-        steps_completed = []
-        plan = []
-        CONVO_SAVE_PATH = os.path.join(SAVE_PATH, f'problem{problem_idx}', f'plan_attempt_{planning_attempts}', f'action{action_i}.pkl')
+        CONVO_SAVE_PATH = os.path.join(SAVE_PATH, f'problem{problem_idx}', f'plan_attempt_{self.planning_attempts}', f'action{action_i}.pkl')
         conversation = deepcopy(plan_sketch_convo)
         variable_description_list = descriptions["skill_variable_descriptions"][action_name]
         grounding_prompt = \
@@ -271,13 +348,15 @@ class LLMAgent:
                 match = re.search("Answer\:\s*[\w\d-]+", response)
                 assert match is not None, response
                 obj_name = response[match.start() + len('Answer:'): match.end()].strip()
-                ground_objs.append([o for o in objects_list if o._str.split(':')[0] == obj_name][0])
+                obj_match = [o for o in objects_list if o._str.split(':')[0] == obj_name]
+                if len(obj_match) == 0:
+                    return None, None
+                ground_objs.append(obj_match[0])
         action_description_info = descriptions["predicates"][action_name]
         action_description, arg_order = action_description_info.split('#')
         ground_action_literal_description = action_description.strip().format(*[ground_objs[int(index)] for index in arg_order.strip()])
-        steps_completed.append(ground_action_literal_description)
         action = self.action_preds[action_name](*ground_objs)
-        return action
+        return action, ground_action_literal_description
  
 
 
@@ -293,10 +372,9 @@ def main(problem_idx, max_actions):
     goal_reached = False
     num_actions = 0
     prev_action = None
-    plan_attempts = 0
     while not goal_reached and num_actions < max_actions:
 
-        action = llm_agent.get_action(obs, problem_idx, plan_attempts)
+        action = llm_agent.get_action(obs, problem_idx)
         print("Executing action ", action)
         next_obs, rew, episode_done, _ = env.step(action)
         positive_effects = {e for e in next_obs.literals - obs.literals}
@@ -304,9 +382,8 @@ def main(problem_idx, max_actions):
         effects = positive_effects | negative_effects
         print("Effects: ", effects)
         if len(effects) == 0:
-            print("Plan failed! Will replan.")
-            llm_agent.reset_plan()
-            plan_attempts += 1
+            print("Plan failed! Replanning.")
+            llm_agent.give_plan_failure_feedback_and_replan(obs, problem_idx)
         obs = next_obs
 
         if action == prev_action:
@@ -320,4 +397,4 @@ def main(problem_idx, max_actions):
             print("Reached goal!")
 
 if __name__ == '__main__':
-    main(2, 30)
+    main(3, 40)
