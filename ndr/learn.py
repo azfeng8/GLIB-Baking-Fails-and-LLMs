f24009f1626b8cad78cb98b2eed306f0ab8b5e4a
azfeng8 <azfeng8@gmail.com>
2025-01-05 13:40:08 -0500

    ALPHA=.5

--- ndr/learn.py
+++ ndr/learn.py
@@ -16,7 +16,7 @@ import logging
 from pprint import pprint
 
 MIXER_ALPHA = 0  # Weight on rule set size penalty
-ALPHA = 0 # Weight on rule set size penalty
+ALPHA = 0.5 # Weight on rule set size penalty
 P_MIN = 1e-11 # Probability for an individual noisy outcome
 VERBOSE = False
 DEBUG = False
