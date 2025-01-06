783106fcd9e4b891af8d284f934a29730bd7ca78
azfeng8 <azfeng8@gmail.com>
2025-01-05 21:34:05 -0500

    update

--- plotting.py
+++ plotting.py
@@ -170,9 +170,9 @@ ALL_TASKS = set(range(22))
 PLOTS = {
     # ("Success Rate on Test Tasks", 'results/Bakingrealistic/bakingrealistic_succ_generalized.png'): GENERALIZATION_TASKS,
     # ("Success Rate on Training Tasks", 'results/Bakingrealistic/bakingrealistic_succ_training.png'): TRAIN_TASKS,
-    (f"{pc.domain}" if pc.domain != "Easygripper" else "Gripper", f'results/{pc.domain}/{pc.domain.lower()}_succ.png'): ALL_TASKS,
+    # (f"{pc.domain}" if pc.domain != "Easygripper" else "Gripper", f'results/{pc.domain}/{pc.domain.lower()}_succ.png'): ALL_TASKS,
     # (f"Keys and Doors", f'results/{pc.domain}/{pc.domain.lower()}_succ.png'): ALL_TASKS,
-    # (f"Baking-Large", f'results/{pc.domain}/{pc.domain.lower()}_succ.png'): ALL_TASKS,
+    (f"Baking-Large", f'results/{pc.domain}/{pc.domain.lower()}_succ.png'): ALL_TASKS,
     # ("Success Rate on Easy Training Tasks", f'results/{pc.domain}/{pc.domain.lower()}_succ_easy_training.png'): EASY_TRAIN_TASKS,
 
 
@@ -627,13 +627,13 @@ def _main():
 
     # # Load the new method results
     new_method_curve_name = "Teacher-GLIB"
-    append_demos[new_method_curve_name] = False
+    # append_demos[new_method_curve_name] = False
     #TODO: when plot student results, change this to True
     append_demos[new_method_curve_name] = True
     for seed in pc.seeds:
         results_path = os.path.join(f'results/{pc.domain}', 'LNDR', 'GLIB_G1', f'{pc.domain}_LNDR_GLIB_G1_interactive_{seed}.pkl')
         #TODO: when plot student results, change this to GLIB_L2
-        results_path = os.path.join(f'results/{pc.domain}', 'LNDR', 'GLIB_L2', f'{pc.domain}_LNDR_GLIB_L2_student_{seed}.pkl')
+        # results_path = os.path.join(f'results/{pc.domain}', 'LNDR', 'GLIB_L2', f'{pc.domain}_LNDR_GLIB_L2_student_{seed}.pkl')
 
         if os.path.exists(results_path):
             with open(results_path, 'rb') as f:
